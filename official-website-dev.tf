072ea4751fdba09a086efe18bb10fc0bfdbdfd5b
Neo Lin <neo.lin@mile.cloud>
2019-08-01 10:43:14 +0800

    first commit

---
+++ official-website-dev.tf
@@ -0,0 +1,9 @@
+# Add the vm-bastionhost instance
+module "vm-bastionhost" {
+  source                 = "./bastionhost"
+  instance_name          = "official-website-dev-bastionhost"
+  instance_zone          = "asia-east1-a"
+  instance_type          = "n1-standard-2"
+  instance_imagetype     = "debian-cloud/debian-9"
+  instance_subnetwork    = ""official-website-public-subnet-1
+}
