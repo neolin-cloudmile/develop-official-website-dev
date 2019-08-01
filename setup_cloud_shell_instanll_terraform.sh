58c62a7ca5aa99dc54b0b9e625695c99681ecf2b
Neo Lin <neo.lin@mile.cloud>
2019-08-01 10:47:05 +0800

    create a script for install Terraform

---
+++ setup_cloud_shell_instanll_terraform.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget https://releases.hashicorp.com/terraform/0.12.5/terraform_0.12.5_linux_amd64.zip
+
+unzip terraform_0.12.5_linux_amd64.zip
+
+export PATH="$PATH:$HOME/terraform"
+cd /usr/bin
+sudo ln -s $HOME/terraform
+cd $HOME
+source ~/.bashrc
+
+terraform --version
+
+export GOOGLE_PROJECT=$(gcloud config get-value project)
+
+echo $GOOGLE_PROJECT
